df1931d22cd8a073b2f026c94b5725fe0b97399a
Sebastian Caicedo <sdrivert@hotmail.com>
2022-05-31 14:39:53 -0500

    Se ha solucionado la parte del background con la técnica del bucle while

--- mishell.c
+++ mishell.c
@@ -10,30 +10,25 @@ int main(int argc, char* argv[]) {
   while (1) {
     printf("> ");
     leer_de_teclado(256, command);
-    int x = strlen(command);
+    int x = 0;
     if (strcmp(command,"salir") == 0) break;
     comando = de_cadena_a_vector(command);
-    int rc = fork();
-      assert(rc >= 0);
-      if (rc == 0){
-        if(command[x-1] == 38){
-          for(int i=2; i >= 0; i--){
-            if(i ==0){
-              comando[0] = NULL;
-              break;
-            }
-            comando[i] = comando[i-1];
-          }   
-          execvp(comando[1], &comando[1]);
-        }else{
-          execvp(comando[0], comando);
-        }
+    while(comando[x]){
+      x++;
+    }
+    pid_t pid = fork();
+    if (!pid){
+      if(strcmp(comando[x-1], "&") == 0){
+        comando[x-1] = '\x0';
+        printf("%s\n", command); 
+      }   
+      execvp(comando[0], comando);
+    }
+    else{
+      if(strcmp(comando[x-1], "&") == 1){
+        wait(NULL);
       }
-      else{
-        if(command[x-1] != 38){
-          wait(NULL);
-        }
-      }
+    }
   }
   return 0;
 }
